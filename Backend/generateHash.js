a2db7385cd9b68e889d593ad8e641cc33d868a85
Vivek Janbandhu <vivekjanbandhu20@gmail.com>
2025-07-04 22:57:57 +0530

    sample env file added for reference

--- Backend/generateHash.js
+++ Backend/generateHash.js
@@ -1,7 +1,7 @@
 // generateHash.js
 import bcrypt from "bcryptjs";
 
-const plainPassword = "Admin@0911"; // <-- Change this to your real password
+const plainPassword = "myAdminPassword"; // <-- Change this to your real password
 
 bcrypt.hash(plainPassword, 10).then(hash => {
   console.log("Hashed password:", hash);
