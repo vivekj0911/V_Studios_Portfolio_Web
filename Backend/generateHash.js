730278cba334ec1dc73940a4f454b1176223695e
Vivek Janbandhu <vivekjanbandhu20@gmail.com>
2025-08-08 22:49:31 +0530

    backend moved to different repo

--- Backend/generateHash.js
+++
@@ -1,8 +0,0 @@
-// generateHash.js
-import bcrypt from "bcryptjs";
-
-const plainPassword = "myAdminPassword"; // <-- Change this to your real password
-
-bcrypt.hash(plainPassword, 10).then(hash => {
-  console.log("Hashed password:", hash);
-});
